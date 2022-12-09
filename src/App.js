ea7278144bd2282a4d9c53a34e3cf80665ab66cb
Abbas Alubeid <abbasalubeid1@gmail.com>
2022-12-09 16:53:46 +0100

    Fixed review button

--- src/App.js
+++ src/App.js
@@ -9,46 +9,44 @@ function App(props) {
 
   const [{steps, run}, setState] = React.useState({
     run : false,
-    steps: [
-      {
-        
-        target: '.hej',
-        content: 'This is my awesome feature!',
-      },
-      {
-        target: '.va',
-        content: 'This another awesome feature!',
-      },
-    ]
+    steps: [],
+    stepIndex : 0
   });
 
 
-  function testRun(){
+  function pageReview(){
     setState({
       run : true,
       steps: [
         {
-          
           target: '.goals',
           content: 'Click here to plan your healthy journey',
+          stepIndex: 0
         },
         {
           target: '.bmi',
           content: 'Click here to calculate your current BMI',
+          stepIndex: 1
         },
         {
           target: '.home',
           content: 'Click here to go back to the homepage',
+          stepIndex: 2
         },
       ]
     })
+  }
+
+  function refreshACB(){
+    window.location.reload();
   }
 
   return (
         <div>
           <div className="app">
             <Joyride steps={steps}
-                      run = {run}/>
+                      run = {run}
+                      continuous ={true}/>
           </div>
           <nav>
             <li>
@@ -61,7 +59,8 @@ function App(props) {
             <Link to="Home" className='home'>Homepage</Link>
             </li>
           </nav>
-          <button onClick={testRun}>TestRun</button>
+          <button onClick={pageReview}>Page review</button>
+          <button onClick={refreshACB}>Restore pageReview</button>
           <Routes>
             <Route path="Home" element={<Homepage/>} />
             <Route path="goals" element={<GoalsSearchPresenter model={props.model}/>}/>
