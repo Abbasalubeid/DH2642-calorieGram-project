60cca402b4c09fbd73668c6296b2edd0f5da599b
Abbas Alubeid <abbasalubeid1@gmail.com>
2022-12-10 18:36:55 +0100

    review button and layout

--- src/App.js
+++ src/App.js
@@ -9,61 +9,74 @@ import "./css/App.css";
 
 function App(props) {
 
-  const [{ steps, run,}, setState] = React.useState({
+  const [state, setState] = React.useState({
     run: false,
-    steps: [],
+    steps: [
+      {
+        target: '.nav-home',
+        content: <h2>Welcome to Calorie Gram🍓
+                     Your personal fitness advisor, calorie calculator, and more 💪 
+                </h2>,
+        locale: { skip: <strong aria-label="skip">SKIP</strong> },
+        placement: 'center',
+      },
+      {
+
+        target: '.nav-home',
+        content: <h2>Click here to go back to the homepage 🏠</h2>,
+        locale: { skip: <strong aria-label="skip">SKIP</strong> },
+      },
+      {
+        target: '.nav-bmi',
+        content: <h2>Calculate your current BMI ✔️</h2>,
+        locale: { skip: <strong aria-label="skip">SKIP</strong> },
+      },
+      {
+        target: '.nav-goals',
+        content: <h2>Calculate your food intake and set a personal goal🎯</h2>,
+        locale: { skip: <strong aria-label="skip">SKIP</strong> },
+      },
+    ],
   });
 
 
   function pageReviewACB() {
-    setState({
-      run: true,
-      steps: [
-        {
-          target: '.nav-home',
-          content: <h2>Welcome to Calorie Gram🍓
-                       Your personal fitness advisor, calorie calculator, and more 💪 
-                  </h2>,
-          locale: { skip: <strong aria-label="skip">SKIP</strong> },
-          placement: 'center',
-        },
-        {
-
-          target: '.nav-home',
-          content: <h2>Click here to go back to the homepage 🏠</h2>,
-          locale: { skip: <strong aria-label="skip">SKIP</strong> },
-        },
-        {
-          target: '.nav-bmi',
-          content: <h2>Calculate your current BMI ✔️</h2>,
-          locale: { skip: <strong aria-label="skip">SKIP</strong> },
-        },
-        {
-          target: '.nav-goals',
-          content: <h2>Calculate your food intake and set a personal goal🎯</h2>,
-          locale: { skip: <strong aria-label="skip">SKIP</strong> },
-        },
-      ]
-    })
+    const newState = {};
+    newState.steps = state.steps;
+    newState.run = true;
+    setState(newState)
   }
 
   function refreshACB() {
-    window.location.reload();
+    const newState = {};
+    newState.steps = state.steps;
+    newState.run = false;
+    setState(newState)
   }
-
 
   return (
     <>
       <div>
-        <Joyride steps={steps}
-          run={run}
+        <Joyride steps={state.steps}
+          run={state.run}
           continuous={true}
-          showSkipButton/>
+          showSkipButton
+          styles={{
+          options  : {
+            arrowColor : '#006dcc',
+            backgroundColor: '#fff',
+            beaconSize: 100,
+            overlayColor: 'rgba(0, 0, 0, 0.5)',
+            primaryColor: '#006dcc',
+            spotlightShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
+            textColor: '#333',
+            zIndex: 100,
+          }}}/>
       </div>
       <NavbarView />
       <div className="mainContainer">
-        <input type="submit" value="Review" className = {run === false ? "btn-submit btn-lg": "hidden"} onClick={pageReviewACB}/>
-        <input type="submit" value="Restore Review" className = {run === true ? "btn-submit btn-lg": "hidden"} onClick={refreshACB}/>
+        <input type="submit" value="Review" className = {state.run === false ? "btn-submit btn-lg": "hidden"} onClick={pageReviewACB}/>
+        <input type="submit" value="Restore Review" className = {state.run === true ? "btn-submit btn-lg": "hidden"} onClick={refreshACB}/>
         <Routes>
           <Route path="home" element={<Homepage />} />
           <Route path="goals" element={<GoalsSearchPresenter model={props.model} />}>
