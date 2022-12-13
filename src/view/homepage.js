36508d56bf73544192651a796359da4d1890bc00
ismailwedihaji <ismailwedihaji@gmail.com>
2022-12-13 18:25:12 +0100

    nothing

--- src/view/homepage.js
+++ src/view/homepage.js
@@ -18,6 +18,7 @@ export default function Homepage() {
         content: <h2>See your current diet plan🥗</h2>,
         locale: { skip: <strong aria-label="skip">SKIP</strong> },
       },
+      
       {
         target: '.nav-bmi',
         content: <h2>Calculate your current BMI ✔️</h2>,
@@ -33,7 +34,9 @@ export default function Homepage() {
         target: '.nav-home',
         content: <h2>Click here to go back to the homepage 🏠</h2>,
         locale: { skip: <strong aria-label="skip">SKIP</strong> },
+       
       },
+      
     ],
   });
 
@@ -43,6 +46,12 @@ export default function Homepage() {
     newState.steps = joyrideState.steps;
     newState.run = true;
     setJoyrideState(newState)
+    if(newState.run == false){
+      const newState = {};
+      newState.steps = joyrideState.steps;
+      newState.run = false;
+      setJoyrideState(newState)
+    }
   }
 
   function refreshACB() {
@@ -78,8 +87,9 @@ export default function Homepage() {
         <p className="anim">
           Welcome to Calorie Gram🍓 Your personal fitness advisor, calorie calculator, and more 💪
         </p>
-        <button onClick={pageReview} className={joyrideState.run === false ? "btn anim" : "hidden"}>Page review</button>
-        <button onClick={refreshACB} className={joyrideState.run === true ? "btn anim" : "hidden"}>Restore pageReview</button>
+        <button onClick={pageReview} className={joyrideState.run === false ? "btn anim" : "hidden"} >Page review</button>
+    
+       
         <img src="apple.png" className="feature-img anim" />
       </div>
     </div >
