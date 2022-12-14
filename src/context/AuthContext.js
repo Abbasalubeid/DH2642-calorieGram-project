77427fe6c651b87e4dcb572420ae248c8cc80f1e
ismailwedihaji <ismailwedihaji@gmail.com>
2022-12-14 17:23:52 +0100

    updated callback

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,40 +1,45 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { auth } from "../model/firebaseModel";
 
 const AuthContext = React.createContext()
 
-export function useAuth(){
+export function useAuth() {
     return useContext(AuthContext)
 }
 
-export function AuthProvider({children}){
+export function AuthProvider({ children }) {
 
-    const[currentUser, setCurrentUser] = React.useState()
+    const [currentUser, setCurrentUser] = React.useState()
 
-    const[loading, setLoading] = React.useState(true)
+    const [loading, setLoading] = React.useState(true)
 
     function signup(email, password) {
-     return auth.createUserWithEmailAndPassword(email, password)   
+        return auth.createUserWithEmailAndPassword(email, password)
     }
-    function login(email, password){
+    function login(email, password) {
         return auth.signInWithEmailAndPassword(email, password)
     }
-   useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(user => { 
-       
+    function rer(user){
         setCurrentUser(user);
         setLoading(false)
-    })
-    return unsubscribe
-   }, [])
+    }
+    
+
+    function unsubscrbedUser(){
+        const unsubscribe = auth.onAuthStateChanged(rer)
+        
+        return unsubscribe
+    }
+
+    useEffect(unsubscrbedUser, [])
     const value = {
         currentUser,
         signup,
         login
     }
 
-    return(
-        <AuthContext.Provider value={value}> 
+    return (
+        <AuthContext.Provider value={value}>
             {!loading && children}
         </AuthContext.Provider>
     )
