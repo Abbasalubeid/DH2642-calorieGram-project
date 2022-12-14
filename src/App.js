b938ce00cfe5e4e8e34efaddef8c7f3330dc0453
ismailwedihaji <ismailwedihaji@gmail.com>
2022-12-14 19:58:49 +0100

    created private route

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App(props) {
           <Route path="signup" element={<RegisterPresenter model={props.model} />} />
          
         </Routes>
-        <LogoutPresenter model={props.model} />
+        <LogoutPresenter/>
       </div>
     </div>);
 
