18c40764f7b7503f235cfe3d90814f3c990b90ff
Bashkim2 <ahmedhaadibashiir@gmail.com>
2022-12-15 16:07:58 +0100

    done with the design for bmi

--- src/presenter/bmiPresenter.js
+++ src/presenter/bmiPresenter.js
@@ -52,7 +52,8 @@ export default function BmiPresenter(props) {
         <div className="bmi-mainStyle">
             <div>
                 <h1>BMI Calculate</h1>
-                <p>The Calorie Calculator can be used to estimate the number of calories a person needs to consume each day.
+                <p className="first-p">The BMI Calculator can be used to estimate the number of calories
+                    a person needs to consume each day.
                     This calculator can also provide some simple guidelines for gaining or losing weight.</p>
             </div>
             <div className="bmi-style">
@@ -81,20 +82,32 @@ export default function BmiPresenter(props) {
 /* custom component */
 function CustomInfo({ href, children, ...props }) {
     return (
-        <div>
+        <div className="custom-info">
+            <input type="checkbox" id="check" />
             <h2>Information</h2>
+            <img src="bmicalc.png" />
             <p>
+                <h4>What's BMI?</h4>
                 Body mass index (BMI) to determine how healthy you are.
                 For most adults, a BMI between 18.5 to 24.9 is the idead BMI to have.
 
                 BMI is not a perfect measure, because it does not directly assess body fat.<br /><br />
-                Muscle and bone are denser than fat, so an athlete or muscular person may have a high BMI, yet not have too much fat. But most people are not athletes,
-                and for most people, BMI is a very good gauge of their level of body fat.<br></br>
+                Muscle and bone are denser than fat, so an athlete or muscular person may have a high BMI,
+                yet not have too much fat. But most people are not athletes,
+                and for most people, BMI is a very good gauge of their level of body fat.
                 <br />
-                What are calories?
-                Calories are a measure of how much energy food or drink contains. The amount of energy you need will depend on: age, how active you are and your hight and weight<br></br><br />
-                The term calorie is commonly used as shorthand for kilocalorie. You will find this written as kcal on food packets. Kilojoules (kJ) are the equivalent of kilocalories within the International System of Units, and you'll see both kJ and kcal on nutrition labels. 4.2kJ is equivalent to approximately 1kcal.
             </p>
-        </div>
+            <div className="readmore-text">
+                <h4>What are calories?</h4>
+                <p>
+                    Calories are a measure of how much energy food or drink contains. The amount of energy you need
+                    will depend on: age, how active you are and your hight and weight.
+                    The term calorie is commonly used as shorthand for kilocalorie. You will find this written as kcal
+                    on food packets. Kilojoules (kJ) are the equivalent of kilocalories within the International System
+                    of Units, and you'll see both kJ and kcal on nutrition labels. 4.2kJ is equivalent to approximately 1kcal.
+                </p>
+            </div>
+            <label for="check">Read More</label>
+        </div >
     )
 }
