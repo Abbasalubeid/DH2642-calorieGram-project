487518534d061f5f52874ab480f7e19a8cc2ff15
Bashkim2 <ahmedhaadibashiir@gmail.com>
2022-12-15 01:46:33 +0100

    added logout bottom and some anim to bmi result

--- src/view/bmiResultView.js
+++ src/view/bmiResultView.js
@@ -3,7 +3,7 @@ import "../css/bmiResult.css";
 export default function BmiResultView(props) {
 
     return (
-        <div className="row">
+        <div className="row anim">
             <div className="col text">Current BMI</div>
             <div className="col state">
                 <span></span>
