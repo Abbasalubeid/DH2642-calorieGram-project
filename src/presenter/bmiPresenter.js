38480f12eda534e3c882104b7d565a4779b75e83
Abbas Alubeid <abbasalubeid1@gmail.com>
2022-12-18 22:57:35 +0100

    Removed comments

--- src/presenter/bmiPresenter.js
+++ src/presenter/bmiPresenter.js
@@ -15,7 +15,6 @@ export default function BmiPresenter(props) {
     const [error, setError] = React.useState(null);
     const [searchParams, setSearchParams] = React.useState({});
     const [show, setShow] = React.useState(false);
-    // const [style, setStyle] = React.useState("");
 
 
 
@@ -69,36 +68,10 @@ export default function BmiPresenter(props) {
     function userChangedBmi(bmi){
         props.model.setUserBmi(bmi)
     }
-    // function chooseColor(color) {
-    //     if(data){
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         if(data.health === "Normal"){
-    //             setStyle("normal");
-    //         }
-    //         else {
-    //             setStyle("");
-    //         }
-    //     }
-    // }
 
 
     React.useEffect(wasCreatedACB, []);
     React.useEffect(promiseHasChangedACB, [promise]);
-    // React.useEffect(chooseColor, [data]);
 
 
     return (
@@ -149,7 +122,7 @@ function CustomInfo({ href, children, ...props }) {
                 <span className="bold-text">What's BMI?</span>
                 <br/>
                 Body mass index (BMI) to determine how healthy you are.
-                For most adults, a BMI between 18.5 to 24.9 is the idead BMI to have.
+                For most adults, a BMI between 18.5 to 24.9 is the ideal BMI to have.
 
                 BMI is not a perfect measure, because it does not directly assess body fat.<br /><br />
                 Muscle and bone are denser than fat, so an athlete or muscular person may have a high BMI,
--- src/view/bmiResultView.js
+++ src/view/bmiResultView.js
@@ -19,7 +19,6 @@ export default function BmiResultView(props) {
             <div id="healthstate" className="col state">
                 <span></span>
                 {props.bmiResult.health}</div>
-                {/* <input type="button" value={props.bmiResult.health} onClick={changeColorCB}></input> */}
         </div >
     );
 }
