6f27faa44c380368b2a99ce77e74315794a0fd1d
Bashkim2 <ahmedhaadibashiir@gmail.com>
2022-12-18 01:44:38 +0100

    deleted goals info

--- src/presenter/goalsSearchPresenter.js
+++ src/presenter/goalsSearchPresenter.js
@@ -123,9 +123,7 @@ export default function GoalsSearchPresenter(props) {
                     
                 />
             </div>
-            <div className={!show ? "goal-info" : "hidden"}>
-                <CustomInfo />
-            </div>
+        
             <div className={show ? "goal-result" : "hidden"}>
                 {promiseNoData({ promise, data, error }) ||
                     <GoalsResultView
@@ -140,39 +138,4 @@ export default function GoalsSearchPresenter(props) {
 
     )
 
-}
-
-
-/* custom component */
-function CustomInfo({ href, children, ...props }) {
-    return (
-        <div className="custom-info anim">
-            <input type="checkbox" id="check" />
-            <h2>Information</h2>
-            <img src="bmicalc.png" />
-            <p>
-                <span className="bold-text">What's BMI?</span>
-                <br />
-                Body mass index (BMI) to determine how healthy you are.
-                For most adults, a BMI between 18.5 to 24.9 is the idead BMI to have.
-
-                BMI is not a perfect measure, because it does not directly assess body fat.<br /><br />
-                Muscle and bone are denser than fat, so an athlete or muscular person may have a high BMI,
-                yet not have too much fat. But most people are not athletes,
-                and for most people, BMI is a very good gauge of their level of body fat.
-                <br />
-            </p>
-            <div className="readmore-text">
-                <h4>What are calories?</h4>
-                <p>
-                    Calories are a measure of how much energy food or drink contains. The amount of energy you need
-                    will depend on: age, how active you are and your hight and weight.
-                    The term calorie is commonly used as shorthand for kilocalorie. You will find this written as kcal
-                    on food packets. Kilojoules (kJ) are the equivalent of kilocalories within the International System
-                    of Units, and you'll see both kJ and kcal on nutrition labels. 4.2kJ is equivalent to approximately 1kcal.
-                </p>
-            </div>
-            <label htmlFor="check">Read More</label>
-        </div >
-    )
 }
