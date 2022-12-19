7346a3b863599e2554e82f98f7ef4da43b3c308b
Abbas Alubeid <abbasalubeid1@gmail.com>
2022-12-19 16:28:28 +0100

    Validation if something changes outside the profile

--- src/presenter/singupPresenter.js
+++ src/presenter/singupPresenter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {  useNavigate } from 'react-router-dom';
 import { UserAuth } from  "../model/AuthContext";
-import Signup from '../view/signupView';
+import Signup from "../view/SignupView"
 import { AuthProvider } from '../model/AuthContext';
 
 export default function SignupPresenter () {
