cb8a9edc449d4e8acf9710e6d4acb0b299a6b60a
Abbas Alubeid <abbasalubeid1@gmail.com>
2022-12-19 16:23:15 +0100

    Validation if something changes outside the profile

--- src/presenter/dietPresenter.js
+++ src/presenter/dietPresenter.js
@@ -48,6 +48,7 @@ export default function DietPresenter(props){
         setTempAge(age);
         setTempWeight(weight)
         setTempHeight(height)
+        setTempGender(gender)
     }
 
     function userSearchedACB(wantToSave) {
